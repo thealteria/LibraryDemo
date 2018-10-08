d1430563a5b81c223996132648687a2c6f2444ab
TheAlteria <sarcasaman@gmail.com>
2018-10-08 19:22:07 +0530

    .

--- app/src/main/java/com/thealteria/librarydemo/MainActivity.java
+++ app/src/main/java/com/thealteria/librarydemo/MainActivity.java
@@ -3,11 +3,15 @@ package com.thealteria.librarydemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.thealteria.toastutil.LatlngAddress;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LatlngAddress.currentAddress();
     }
 }
